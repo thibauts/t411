5cf66bc8de4aadaf45ba28afffe1f5641d3c32c1
thibauts <thibaut.seguy@gmail.com>
2015-05-01 21:26:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+t411
+====
+### T411 API client
+
+A client for the T411 API that works both in node and the browser with [browserify](http://browserify.org).
+
+Install
+-------
+
+```bash
+$ npm install t411
+```
+
+Usage
+-----
+
+```javascript
+var T411 = require('t411');
+
+var client = new T411();
+
+client.auth('your-username', 'your-password', function(err) {
+  if(err) throw err;
+
+  // search torrents
+  client.search('interstellar', function(err, result) {
+    if(err) throw err;
+    console.log(result);
+  });
+
+  // download and parse a .torrent
+  client.download(id, function(err, buf) {
+    // `buf` is a Buffer in node as well as in the browser
+    var parsed = require('parse-torrent')(buf);
+    console.log(parsed);
+  });
+
+  // All the methods provided by the API are implemented (categories,
+  // terms, tops, bookmarks). For advanced use check the source code, 
+  // it should be pretty easy to read :)
+});
+```
---
+++ index.js
@@ -0,0 +1,212 @@
+
+var request = require('superagent');
+var concat = require('concat-stream');
+var toBuffer = require('typedarray-to-buffer');
+
+var API_HOST = 'api.t411.io';
+
+function Client() {
+  this.uid = null;
+  this.token = null;
+}
+
+
+Client.prototype.url = function(endpoint) {
+  return 'https://' + API_HOST + endpoint;
+};
+
+
+Client.prototype.get = function(uri, query, callback) {
+  if(typeof query === 'function') {
+    callback = query;
+    query = null;
+  }
+
+  var req = request.get(this.url(uri));
+
+  if(this.token) {
+    req.set('Authorization', this.token);
+  }
+
+  if(query) {
+    req.query(query);
+  }
+
+  req.end(responseHandler(callback));
+};
+
+
+Client.prototype.getBinary = function(uri, callback) {
+  var req = request.get(this.url(uri));
+
+  if(this.token) {
+    req.set('Authorization', this.token);
+  }
+
+  if(typeof window !== 'undefined') {
+    // If we're in the browser, we ask for an ArrayBuffer
+    req.on('request', function() {
+      this.xhr.responseType = 'arraybuffer';
+    });
+  }
+
+  req.end(responseHandler(callback));
+};
+
+
+Client.prototype.post = function(uri, data, callback) {
+  var req = request.post(this.url(uri));
+
+  if(this.token) {
+    req.set('Authorization', this.token);
+  }
+
+  req
+    .type('application/x-www-form-urlencoded')
+    .send(data)
+    .end(responseHandler(callback));
+};
+
+
+Client.prototype.del = function(uri, callback) {
+  // DELETE isn't allowed by the preflight response
+  // CORS headers, but POST seems to work
+  var req = request.post(this.url(uri));
+
+  if(this.token) {
+    req.set('Authorization', this.token);
+  }
+
+  req.end(responseHandler(callback));
+}
+
+
+Client.prototype.auth = function(username, password, callback) {
+  var self = this;
+
+  var data = {
+    username: username,
+    password: password
+  };
+
+  this.post('/auth', data, function(err, data) {
+    if(err) return callback(err);
+    self.uid = data.uid;
+    self.token = data.token;
+    callback();
+  });
+};
+
+
+Client.prototype.profile = function(uid, callback) {
+  this.get('/users/profile/' + uid, callback);
+};
+
+
+Client.prototype.categories = function(callback) {
+  this.get('/categories/tree', callback);
+};
+
+
+Client.prototype.terms = function(callback) {
+  this.get('/terms/tree', callback);
+};
+
+
+Client.prototype.search = function(query, options, callback) {
+  if(typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+
+  this.get('/torrents/search/' + query, { query: options }, callback);
+};
+
+
+Client.prototype.download = function(id, callback) {
+  return this.getBinary('/torrents/download/' + id, callback);
+};
+
+
+Client.prototype.top = function(type, callback) {
+  this.get('/torrents/top/' + type, callback);
+};
+
+
+Client.prototype.top100 = function(callback) {
+  this.top('100', callback);
+};
+
+
+Client.prototype.topToday = function(callback) {
+  this.top('today', callback);
+};
+
+
+Client.prototype.topWeek = function(callback) {
+  this.top('week', callback);
+};
+
+
+Client.prototype.topMonth = function(callback) {
+  this.top('month', callback);
+};
+
+
+Client.prototype.bookmarks = function(callback) {
+  this.get('/bookmarks', callback);
+};
+
+
+Client.prototype.createBookmark = function(id, callback) {
+  this.post('/bookmarks/save/' + id, {}, callback);
+};
+
+
+Client.prototype.deleteBookmark = function(id, callback) {
+  ids = Array.isArray(id)
+    ? id.join(',')
+    : id;
+
+  this.del('/bookmarks/delete/' + ids, callback);
+}
+
+
+function responseHandler(callback) {
+  return function(err, res) {
+    if(err) return callback(err);
+
+    if(res.type === 'application/x-bittorrent') {
+
+      if(typeof window === 'undefined') {
+        // In node, collect the response data in a Buffer and return it
+        res.pipe(concat(function(data) {
+          callback(null, data);
+        }));
+        return;
+      }
+
+      // In the browser, return the response ArrayBuffer
+      return callback(null, res.body);
+    }
+
+    var data = JSON.parse(res.text);
+
+    if(data.error) {
+      var err = new Error(data.error);
+      err.code = data.code;
+      return callback(err);
+    }
+
+    callback(null, data);
+  };
+}
+
+
+request.parse['application/x-bittorrent'] = function(data) {
+  // This is called when in the browser only
+  return toBuffer(new Uint8Array(data));
+};
+
+
+module.exports = Client;
